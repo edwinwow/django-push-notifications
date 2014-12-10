3de85ec4f719e9e6df94e1199a0ea12d775798c0
edwinwow <yunlong.he@hotmail.com>
2014-12-10 22:28:13 +0800

    Update api.py

--- push_notifications/api.py
+++ push_notifications/api.py
@@ -1,42 +1,75 @@
-from tastypie.authorization import Authorization
-from tastypie.authentication import BasicAuthentication
-from tastypie.resources import ModelResource
+from rest_framework import serializers, permissions, viewsets
 from .models import APNSDevice, GCMDevice
 
 
-class APNSDeviceResource(ModelResource):
-	class Meta:
-		authorization = Authorization()
-		queryset = APNSDevice.objects.all()
-		resource_name = "device/apns"
+class IsOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to see it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Only allow the user to see their own devices
+        return obj.user == request.user
 
 
-class GCMDeviceResource(ModelResource):
-	class Meta:
-		authorization = Authorization()
-		queryset = GCMDevice.objects.all()
-		resource_name = "device/gcm"
+class APNSDeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = APNSDevice
+        fields = ('name', 'device_id', 'registration_id')
+
+    read_only = ('date_created',)
 
 
-class APNSDeviceAuthenticatedResource(APNSDeviceResource):
-	# user = ForeignKey(UserResource, "user")
+class GCMDeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GCMDevice
+        fields = ('name', 'device_id', 'registration_id')
 
-	class Meta(APNSDeviceResource.Meta):
-		authentication = BasicAuthentication()
-		# authorization = SameUserAuthorization()
-
-	def obj_create(self, bundle, **kwargs):
-		# See https://github.com/toastdriven/django-tastypie/issues/854
-		return super(APNSDeviceAuthenticatedResource, self).obj_create(bundle, user=bundle.request.user, **kwargs)
+    read_only = ('date_created',)
 
 
-class GCMDeviceAuthenticatedResource(GCMDeviceResource):
-	# user = ForeignKey(UserResource, "user")
+class DeviceViewSetMixin(object):
+    lookup_field = 'registration_id'
+    queryset = APNSDevice.objects.all()
+    serializer_class = APNSDeviceSerializer
+    permission_classes = (permissions.IsAuthenticated,
+                          IsOwner,)
 
-	class Meta(GCMDeviceResource.Meta):
-		authentication = BasicAuthentication()
-		# authorization = SameUserAuthorization()
+    def pre_save(self, obj):
+        # Set the requesting user as the device user
+        obj.user = self.request.user
 
-	def obj_create(self, bundle, **kwargs):
-		# See https://github.com/toastdriven/django-tastypie/issues/854
-		return super(GCMDeviceAuthenticatedResource, self).obj_create(bundle, user=bundle.request.user, **kwargs)
+
+class APNSDeviceViewSet(DeviceViewSetMixin, viewsets.ModelViewSet):
+    queryset = APNSDevice.objects.all()
+    serializer_class = APNSDeviceSerializer
+
+
+class GCMDeviceViewSet(DeviceViewSetMixin, viewsets.ModelViewSet):
+    queryset = GCMDevice.objects.all()
+    serializer_class = GCMDeviceSerializer
+
+
+#
+# Links for viewsets
+#
+apns_list = APNSDeviceViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+apns_detail = APNSDeviceViewSet.as_view({
+    'get': 'retrieve',
+    'delete': 'destroy'
+})
+
+gcm_list = GCMDeviceViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+gcm_detail = GCMDeviceViewSet.as_view({
+    'get': 'retrieve',
+    'delete': 'destroy'
+})
+
